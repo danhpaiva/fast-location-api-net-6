e4f6f28009c7cb24efd6cf6bb8fd96f9e2f29412
Paiva <danpaiva@live.com>
2023-11-10 13:44:34 -0300

    feat: add class model Produto

---
+++ FastLocationAPI/Models/Produto.cs
@@ -0,0 +1,16 @@
+﻿namespace FastLocationAPI.Models;
+public class Produto
+{
+    public int ProdutoId { get; set; }
+    public string? NfcId { get; set; }
+    public string? Codigo { get; set; }
+    public string? Nome { get; set; }
+    public string? Descricao { get; set; }
+    public int Quantidade { get; set; }
+    public DateTime DataCadastro { get; set; }
+    public DateTime DataAtualizacao { get; set; }
+    public string? Localizacao { get; set; }
+    public string? Responsavel { get; set; }
+    public bool Calibragem { get; set; }
+    public bool Ativo { get; set; }
+}
